e928e38f2b33c5dc4c4746082ba43a2e64577bf2
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-01-29 02:36:30 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -21,8 +21,8 @@ class School
 
   def sort
     sorted = {}
-    roster.sort do |grade, students|
-      sorted.grade = students.sort
+    roster.each do |grade, student|
+      sorted.grade = student.sort
     end
     sorted
   end
